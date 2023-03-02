135f24bbcfa560292e78eeb822dde0e63a4c8fa0
Turbo <dev@turbo.ooo>
2023-03-02 12:12:03 -0500

    Initial commit

---
+++ plugins/community.lua
@@ -0,0 +1,11 @@
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.rose-pine" },
+  { import = "astrocommunity.colorscheme.kanagawa" },
+  { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.media.presence-nvim" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.python" },
+}
